22217eaeefbe6d99f7672b73d699aa6e01c29f31
Vito Castellano <castellano.vito@gmail.com>
2019-04-14 18:05:08 +0200

    Removed useless server in bot

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"bitbucket.org/no-name-game/no-name/app"
-	"bitbucket.org/no-name-game/no-name/web"
 )
 
 func main() {
-	// Server - Only for ping
-	go web.Run()
-
-	// Game - NoName
 	app.Run()
 }
