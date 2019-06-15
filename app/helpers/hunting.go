42bf7d874111efb871fe433b8726d4cac8b5ff6a
Lorenzo Avallone <lorenzo.avallone01@gmail.com>
2019-06-15 14:33:32 +0200

    Fix no weapon equipped error

--- app/helpers/hunting.go
+++ app/helpers/hunting.go
@@ -1,21 +1,20 @@
 package helpers
 
 import (
-	"bitbucket.org/no-name-game/no-name/app/acme/nnsdk"
 	"bitbucket.org/no-name-game/no-name/app/provider"
 	"bitbucket.org/no-name-game/no-name/services"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-func GenerateWeaponKeyboard(player nnsdk.Player) (keyboardRows [][]tgbotapi.KeyboardButton) {
+func GenerateWeaponKeyboard() (keyboardRows [][]tgbotapi.KeyboardButton) {
 	// FIXME: remove this
-	weapons, err := provider.GetPlayerWeapons(player, "true")
+	weapons, err := provider.GetPlayerWeapons(Player, "true")
 	if err != nil {
 		services.ErrorHandler("Cant get weapon equipped", err)
 	}
 
 	for _, weapon := range weapons {
-		keyboardRow := tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton(Trans("combat.attack_with", player.Language.Slug) + weapon.Name))
+		keyboardRow := tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton(Trans("combat.attack_with", weapon.Name)))
 		keyboardRows = append(keyboardRows, keyboardRow)
 	}
 
