9858e74295808e9739f623de4b522532a10d17f1
Vito Castellano <castellano.vito@gmail.com>
2019-06-27 21:29:19 +0200

    Added authentication

--- services/nn-sdk.go
+++ services/nn-sdk.go
@@ -1,9 +1,12 @@
 package services
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"bitbucket.org/no-name-game/no-name/app/acme/nnsdk"
+	_ "github.com/joho/godotenv/autoload" // Autload .env
 )
 
 var (
@@ -13,17 +16,27 @@ var (
 
 // NnSDKUp
 func NnSDKUp() {
-	token := "YOUR-TOKEN"
-
 	var err error
-	NnSDK, err = nnsdk.NewNoNameSDK(token)
+	NnSDK, err = nnsdk.NewNoNameSDK()
 	if err != nil {
 		ErrorHandler("Error connecting NoName WS", err)
 	}
 
 	// Set Host and API version
-	NnSDK.SetWSHost("http://localhost:8080")
-	NnSDK.SetAPIVersion("v1")
+	NnSDK.SetWSHost(fmt.Sprintf("%s:%s", os.Getenv("WS_HOST"), os.Getenv("WS_PORT")))
+	NnSDK.SetAPIVersion(os.Getenv("WS_API_VERSION"))
+	NnSDK.SetAuth(
+		nnsdk.NoNameSDKAuthentication{
+			Username: os.Getenv("WS_USER"),
+			Password: os.Getenv("WS_PASSWORD"),
+		},
+	)
+
+	// Autenticate to WS
+	_, err = NnSDK.Authenticate()
+	if err != nil {
+		log.Panicln("NNSDK - Authentication error", err)
+	}
 
 	// if appDebug := os.Getenv("APP_DEBUG"); appDebug != "false" {
 	// 	NnSDK.Debug = true
