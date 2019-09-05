e0e0469849d63cf7ec32c9686b354716e79ad09a
Lorenzo Avallone <lorenzo.avallone01@gmail.com>
2019-09-05 16:43:34 +0200

    Add player view

--- app/helpers/map.go
+++ app/helpers/map.go
@@ -9,24 +9,38 @@ import (
 )
 
 func TextDisplay(m nnsdk.Map) string {
-	result := "┏━━━━━━━━━━━━━━━━━━┓\n"
+	result := "+---------------------+\n"
 	var cellMap [66][66]bool
 	err := json.Unmarshal([]byte(m.Cell), &cellMap)
 	if err != nil {
 		services.ErrorHandler("UnMarshal Error", err)
 	}
-	for x := 15; x < 26; x++ {
-		result += "┃"
-		for y := 15; y < 36; y++ {
-			if cellMap[x][y] {
-				result += "#"
+	//log.Println("Player X: ", m.PlayerX, " Y: ", m.PlayerY)
+	for x := m.PlayerX - 5; x < m.PlayerX+5; x++ { //11
+		result += "|"
+		for y := m.PlayerY - 10; y < m.PlayerY+11; y++ { // 21
+			if (x >= 0 && x < 66) && (y >= 0 && y < 66) { // In bounds
+				if cellMap[x][y] {
+					result += "#"
+				} else {
+					if x == m.PlayerX && y == m.PlayerY {
+						result += "@"
+					} else if x == m.EnemyX && y == m.EnemyY {
+						result += "*"
+					} else {
+						result += " "
+					}
+
+				}
 			} else {
-				result += " "
+				//log.Println("Out of Bound X: ", x, " Y: ", y)
+				result += "#"
 			}
+
 		}
-		result += "┃"
+		result += "|"
 		result += "\n"
 	}
-	result += "┗━━━━━━━━━━━━━━━━━━┛"
+	result += "+---------------------+"
 	return result
 }
