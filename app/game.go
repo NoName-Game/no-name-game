98f28e2cb697503c33b428ea987d67b5dbbbb17a
Vito Castellano <castellano.vito@gmail.com>
2020-03-01 01:05:52 +0100

    reintrodotta gestione multiworker

--- app/game.go
+++ app/game.go
@@ -71,24 +71,21 @@ func Run() {
 	// Gestisco update ricevuti
 	for update := range updates {
 		// Gestisco singolo update in worker dedicato
-		// go handleUpdate(update)
-		handleUpdate(update)
+		go handleUpdate(update)
+		// handleUpdate(update)
 	}
 }
 
 // handleUpdate - Gestisco singolo update
 func handleUpdate(update tgbotapi.Update) {
 	// Differisco controllo panic/recover
-	// defer func() {
-	// 	// Nel caso in cui panicasse
-	// 	if err := recover(); err != nil {
-	// 		// Registro errore
-	// 		services.ErrorHandler("recover handle update", err.(error))
-	// 		// Mando un messaggio dicendogli di inserire un username
-	// 		// msg := services.NewMessage(update.Message.Chat.ID, Trans("en", "miss_username"))
-	// 		// services.SendMessage(msg)
-	// 	}
-	// }()
+	defer func() {
+		// Nel caso in cui panicasse
+		if err := recover(); err != nil {
+			// Registro errore
+			services.ErrorHandler("recover handle update", err.(error))
+		}
+	}()
 
 	var err error
 	// Gestisco utente
@@ -100,5 +97,4 @@ func handleUpdate(update tgbotapi.Update) {
 
 	// Gestisco update
 	routing(player, update)
-
 }
