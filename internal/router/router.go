c4e71d6c009ceb7e970fd3fc5461afc48ca8cc45
Vito Castellano <castellano.vito@gmail.com>
2020-10-13 01:03:38 +0200

    Implementato mercante

--- internal/router/router.go
+++ internal/router/router.go
@@ -46,6 +46,7 @@ var routes = map[string]reflect.Type{
 	// Safe Planet
 	"route.safeplanet.bank":                               reflect.TypeOf((*controllers.SafePlanetBankController)(nil)).Elem(),
 	"route.safeplanet.crafter":                            reflect.TypeOf((*controllers.SafePlanetCrafterController)(nil)).Elem(),
+	"route.safeplanet.dealer":                             reflect.TypeOf((*controllers.SafePlanetDealerController)(nil)).Elem(),
 	"route.safeplanet.accademy":                           reflect.TypeOf((*controllers.SafePlanetAccademyController)(nil)).Elem(),
 	"route.safeplanet.coalition":                          reflect.TypeOf((*controllers.SafePlanetCoalitionController)(nil)).Elem(),
 	"route.safeplanet.coalition.mission":                  reflect.TypeOf((*controllers.SafePlanetMissionController)(nil)).Elem(),
