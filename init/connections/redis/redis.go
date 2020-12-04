e6c6d09387e4f1d7def5fbd8724056ec808a0807
Vito Castellano <castellano.vito@gmail.com>
2020-12-05 00:29:38 +0100

    Recuperati correttamente valori env per conf redis

--- init/connections/redis/redis.go
+++ init/connections/redis/redis.go
@@ -1,6 +1,10 @@
 package redis
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+
 	goredis "github.com/go-redis/redis"
 	"github.com/sirupsen/logrus"
 )
@@ -12,10 +16,28 @@ type Redis struct {
 
 // Redis Init
 func (redis *Redis) Init() {
+	redisHost := os.Getenv("REDIS_HOST")
+	if redisHost == "" {
+		logrus.WithField("error", fmt.Errorf("missing redis host")).Fatal("[*] Redis Connection: KO!")
+	}
+
+	redisPort := os.Getenv("REDIS_PORT")
+	if redisPort == "" {
+		logrus.WithField("error", fmt.Errorf("missing redis port")).Fatal("[*] Redis Connection: KO!")
+	}
+
+	redisDB := os.Getenv("REDIS_DB")
+	if redisDB == "" {
+		logrus.WithField("error", fmt.Errorf("missing redis db")).Fatal("[*] Redis Connection: KO!")
+	}
+
+	// Converto indice DB
+	redisDBIndex, _ := strconv.Atoi(redisDB)
+
 	redis.Connection = goredis.NewClient(&goredis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
+		Addr:     fmt.Sprintf("%s:%s", redisHost, redisPort),
+		Password: "",           // no password set
+		DB:       redisDBIndex, // use default DB
 	})
 
 	if _, err := redis.Connection.Ping().Result(); err != nil {
