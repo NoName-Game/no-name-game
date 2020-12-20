f86f3bf823abdf779a4bc0b9ede7d78db89aab1b
Vito Castellano <castellano.vito@gmail.com>
2020-12-20 03:14:48 +0100

    NNT-116 - Init funzionalità gioco 3 carte

--- internal/controllers/menu.go
+++ internal/controllers/menu.go
@@ -344,6 +344,10 @@ func (c *MenuController) SafePlanetKeyboard() [][]tgbotapi.KeyboardButton {
 	}
 
 	keyboardRow = append(keyboardRow, []tgbotapi.KeyboardButton{
+		tgbotapi.NewKeyboardButton(helpers.Trans(c.Player.Language.Slug, "route.safeplanet.relax")),
+	})
+
+	keyboardRow = append(keyboardRow, []tgbotapi.KeyboardButton{
 		tgbotapi.NewKeyboardButton(helpers.Trans(c.Player.Language.Slug, "route.safeplanet.accademy")),
 		tgbotapi.NewKeyboardButton(helpers.Trans(c.Player.Language.Slug, "route.safeplanet.hangar")),
 	})
